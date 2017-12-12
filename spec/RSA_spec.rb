8fd936ddd242681b59cd70acb19a354ac51f5bf7
StefchoG <ste.grozev.tues19@gmail.com>
2017-12-12 23:23:55 +0200

    homework 1/4

--- spec/RSA_spec.rb
+++ spec/RSA_spec.rb
@@ -1,10 +1,21 @@
 require 'RSA'
 
 RSpec.describe RSA do 
-	it "initializes instance of RSA with n,e,d integers." do
-		expect
+	it "initializes instance of RSA with n integer." do
+		n = RSA.new
+		expect(n.initialize()).to eq 0
 	end
-	it "" do
-
+	it "initializes instance of RSA with e integer." do
+		e = RSA.new
+		expect(e.initialize()).to eq 0
+	end
+	it "initializes instance of RSA with d integer." do
+		d = RSA.new
+		expect(d.initialize()).to eq 0
+	end
+	
+	it "returns the value of 'n' passed in the initialize" do
+		n_ = RSA.new
+		expect(n_.n()).to eq @n
 	end
 end
